ea0c7215b396d8c1fce335ee549868804589f4ba
bahadir-hamza <bahadirhamza2@gmail.com>
2022-10-28 22:23:11 +0300

    Create odev.py

---
+++ odev/odev.py
@@ -0,0 +1,20 @@
+sehir = "Ankara"
+sehir = "İstanbul"
+
+print(sehir != "İstanbul")
+print(sehir == "İstanbul")
+#sehir değişkeni ikinciye İstanbul değeri atandığından == operatörü True, =! operatörü False değer döndürecektir.
+
+veliAdi = "Ahmet WAKDEA"
+ogrAdi = "Mehmet WAKDEA"
+devamsizlik = 900
+
+print("Sn. "+veliAdi+", "+ogrAdi+" öğrencinizin toplam devamsızlığı ", devamsizlik, " gündür. Piton Lisesi")
+
+####
+
+abnNumarasi = 64386
+donem = 102022
+tutar = 1839*1
+
+print(abnNumarasi, " nolu abonemiz, ", donem, " dönemi faturanız ", tutar, "TL'dir. Pide Belediyesi")
